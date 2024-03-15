986e8a0ed31a421ccbfb1f5e48459ac1c42a3c92
D4rkGhost <hungnhan1236@gmail.com>
2024-03-15 20:18:20 +0700

    NewUpdata

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,10 @@
 // export const API_ROOT = 'https://trelloapp-skv7.onrender.com'
-export const API_ROOT = 'https://localhost:8017'
+let apiRoot = ''
+console.log(process.env)
+if (process.env.BUILD_MODE==='dev') {
+  apiRoot = 'http://localhost:8017'
+}
+if (process.env.BUILD_MODE==='production') {
+  apiRoot = 'https://trelloapp-skv7.onrender.com'
+}
+export const API_ROOT = apiRoot
